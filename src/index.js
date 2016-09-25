3b8672782f8974f696715f75ac34bced1ad80521
michaelaweinreb <michaela.weinreb@hotmail.com>
2016-09-25 21:40:55 +1000

    second commit

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDWpKo2_CJm94K1qV4_FvGGU3q-FngM25U';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+
 
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //this.setState({ videos: videos });
+    });
+  }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
